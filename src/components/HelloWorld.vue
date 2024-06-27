abc5857b5ddc855bad15937c7c1484abd037bc3e
permalik <permalik@protonmail.com>
2024-06-26 21:31:11 -0500

    feat
    init header

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
+
 defineProps<{
-  msg: string
+  logoContent: string
 }>()
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+    <img src="../assets/icon/logo_icon_xs_black_svg.svg" alt="malik studio hand logo icon"/>
+    <p>{{ logoContent }}</p>
   </div>
 </template>
